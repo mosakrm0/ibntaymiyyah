a7d8f1c50e347f0653532ee8f153769bd53f118a
mosakrm0 <130869897+mosakrm0@users.noreply.github.com>
2025-01-31 12:24:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA7GRyVNbJr6teTwEzkkxr-Mg6BByGQt14",
+  apiKey: "-",
   authDomain: "ibntaymiyyah.firebaseapp.com",
   projectId: "ibntaymiyyah",
   storageBucket: "ibntaymiyyah.firebasestorage.app",
